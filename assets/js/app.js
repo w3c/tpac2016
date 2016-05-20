39f2eef37411e136803283f52945ff8eb91476c6
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-20 18:49:07 -0400

    add animation dropdown schedule items

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,3 @@
 (function () {
-    var newsBox = document.getElementById('news-box');
-    var newsBoxCloseBtn = document.getElementById('news-box-close-btn');
-
-    newsBoxCloseBtn.addEventListener('click', function() {
-        newsBox.style.display = 'none';
-    });
+    
 })()
--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -3,6 +3,26 @@
     var scheduleNavigationBtns = document.getElementsByClassName('schedule-navigation-btn');
     var activeScheduleNavigationBtn = scheduleNavigationBtns[0];
     var activeScheduleElement = document.getElementById(scheduleNavigationBtns[0].innerHTML.toLowerCase() + '-schedule');
+
+    var expendScheduleBtns = document.getElementsByClassName('item-dropdown');
+
+    for (var i = 0; i < expendScheduleBtns.length; i++) {
+        (function () {
+
+            var btn = expendScheduleBtns[i];
+
+            expendScheduleBtns[i].addEventListener('click', function() {
+                if(btn.parentNode.className.indexOf('expand') >= 0) {
+                    btn.parentNode.className = btn.parentNode.className.replace(' expand', '')
+                } else {
+                    btn.parentNode.className += ' expand';
+                }
+
+            }, false);
+
+        }());
+    }
+
     loadSchedule(scheduleNavigationBtns[0].innerHTML.toLowerCase());
 
     for (var i = 0; i < scheduleNavigationBtns.length; i++) {
